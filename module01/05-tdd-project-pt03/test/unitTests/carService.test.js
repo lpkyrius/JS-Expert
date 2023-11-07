9cda794431e6df743921fdb4c1a12ddfc779a6e1
lpkyrius <lpkyrius@hotmail.com>
2023-11-07 10:15:44 +0000

    Module 01 - 05 TDD & BDD - Parte 03 3rd step test

--- module01/05-tdd-project-pt03/test/unitTests/carService.test.js
+++ module01/05-tdd-project-pt03/test/unitTests/carService.test.js
@@ -1,6 +1,6 @@
 const { describe, it, before, beforeEach, afterEach } = require('mocha');
 const CarService = require('../../src/service/carService');
-
+const Transaction = require('./../../src/entities/transaction');
 const { join } = require('path');
 const { expect } = require('chai');
 const sinon = require('sinon');
@@ -88,6 +88,12 @@ describe('CarService Suite Tests', () => {
         // age: 50 -> tax: 1.3 -> categoryPrice: 37.6
         // 37.6 * 1.3 = 48.88 * 5 days = 244.40
 
+        // Do not lay in external data
+        sandbox.stub(
+            carService,
+            "taxesBasedOnAge" // can't use .taxBasedOnAge. So, keep it between "" as string
+        ).get(() => [{ from: 40, to: 50, then: 1.3 }])
+
         const expected = carService.currencyFormat.format(244.40);
         const result = carService.calculateFinalPrice(
             customer,
@@ -97,4 +103,42 @@ describe('CarService Suite Tests', () => {
 
         expect(result).to.be.deep.equal(expected);
     });
+
+    it('given a customer and a car category it should return a transaction receipt', async () => {
+        const car = mocks.validCar;
+        // now we have to change more than one property
+        // so, instead of Object.Create() we use rest spread ...mocks
+        // to receive all the properties already in mocks 
+        // and change the ones we need
+        const carCategory = {
+            ...mocks.validCarCategory,
+            price: 37.6,
+            carIds: [car.id]
+        };
+
+        const customer = Object.create(mocks.validCustomer);
+        customer.age = 20;
+
+        const numberOfDays = 5;
+        const dueDate = "10 de novembro de 2020"
+
+        const now = new Date(2020, 10, 5); // month=10 (november) because JS starts with 0, not 1!
+        sandbox.useFakeTimers(now.getTime());
+        // age: 20, tax: 1.1, price: 37.6
+        // 37.6 * 1.1 = 41.36 * 5 = 206.8
+        const expectedAmount = carService.currencyFormat.format(206.8);
+        const result = await carService.rent(
+            customer, carCategory, numberOfDays
+        );
+
+        const expected = new Transaction({
+            customer,
+            car,
+            dueDate,
+            amount: expectedAmount,
+        });
+
+        expect(result).to.be.deep.equal(expected);
+
+    })
 })
