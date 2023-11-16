e0900e1bdcd6a5d6c25b3327281a84cf6de155f5
lpkyrius <lpkyrius@hotmail.com>
2023-11-16 11:49:12 +0000

    Module 04 - Reflect

--- module04/05-reflect/reflect.js
+++ module04/05-reflect/reflect.js
@@ -1,79 +1,76 @@
+// REFLECT intercepts functions to add behaviors.
+// Goal: To ensure the Object's semantics and security.
+
 'use strict'
 
-const { deepEqual } = require('assert')
-const assert = require('assert')
+const assert = require('assert');
 
-// garantir semantica e segurança em objetos
-
-// ---- apply
+// --- apply
 const myObj = {
-    add(myValue) {
-        return this.arg1 + this.arg2 + myValue
+    add(myValue){
+        // 'this' does not exist yet. So, who calls this function must pass the 'this'
+        return this.arg1 + this.arg2 + myValue;
     }
 }
-// Function.prototype.apply = () => { throw new TypeError('Eita!')}
-// myObj.add.apply = function () { throw new Error('Vixxx')}
 
-assert.deepStrictEqual(myObj.add.apply({ arg1: 10, arg2: 20 }, [100]), 130)
+// This is rare but can happen: replace the apply function:
+// Function.prototype.apply = () => { throw new TypeError('Eita!')};
 
-// um problema que pode acontecer (raro)
-// Function.prototype.apply = () => { throw new TypeError('Eita!')}
+// This one is not so rare:
+// myObj.add.apply = function () { throw new TypeError('Oops')};
+assert.deepStrictEqual(myObj.add.apply({ arg1: 10, arg2: 20 }, [100]), 130);
 
-// esse aqui pode acontecer!
-myObj.add.apply = function () { throw new TypeError('Vixxx') }
-
+myObj.add.apply = function () { throw new TypeError('Oops')};
 assert.throws(
     () => myObj.add.apply({}, []),
     {
         name: "TypeError",
-        message: 'Vixxx'
+        message: 'Oops'
     }
 )
 
-// usando reflect:
-const result = Reflect.apply(myObj.add, { arg1: 40, arg2: 20}, [200])
-assert.deepStrictEqual(result, 260)
+// --- Using Reflect
+// Even with the changed apply() above, this next line works because Reflect has a different approach
+const result = Reflect.apply(myObj.add, { arg1: 40, arg2: 20 }, [200]);
+assert.deepStrictEqual(result, 260);
+
 // ---- apply
 
 // --- defineProperty
 
-// questoes semanticas
+// semantics questions
 function MyDate() {}
 
-// feio pra Kct, tudo é Object, mas Object adicionando prop para uma function?
+// bad code, ugly! 
 Object.defineProperty(MyDate, 'withObject', { value: () => 'Hey there'})
-
-// agora faz mais sentido
+// now it's gonna be better:
 Reflect.defineProperty(MyDate, 'withReflection', { value: () => 'Hey dude'})
-
-assert.deepStrictEqual(MyDate.withObject(), 'Hey there')
-assert.deepStrictEqual(MyDate.withReflection(), 'Hey dude')
+assert.deepStrictEqual(MyDate.withObject(), 'Hey there');
+assert.deepStrictEqual(MyDate.withReflection(), 'Hey dude');
 // --- defineProperty
 
 // --- deleteProperty
-const withDelete = { user: 'ErickWendel'}
-// imperformático, evitar ao máximo
+const withDelete = { user: 'LeandroPassos'};
+// non-performative, avoid as possible
 delete withDelete.user
+assert.deepStrictEqual(withDelete.hasOwnProperty('user'), false);
 
-assert.deepStrictEqual(withDelete.hasOwnProperty('user'), false)
-
-const withReflection = { user: 'XuxaDaSilva'}
-Reflect.deleteProperty(withReflection, "user")
-assert.deepStrictEqual(withReflection.hasOwnProperty("user"), false)
-
+const withReflection = { user: 'XuxaDaSilva' };
+Reflect.deleteProperty(withReflection, "user");
+assert.deepStrictEqual(withReflection.hasOwnProperty("user"), false);
 // --delete Property
 
 //  ---- get
 
-// Deveriamos fazer um get somente em instancias de referencia
-assert.deepStrictEqual(1['userName'], undefined)
-// com reflection, uma exceçao é lançada!
+// We should use get only in reference instances
+assert.deepStrictEqual(1['userName'], undefined) 
+// using Reflection, we will get an exception
 assert.throws(() => Reflect.get(1, "userName"), TypeError)
 //  ---- get
 
 //  --- has
-assert.ok('superman' in { superman: ''})
-assert.ok(Reflect.has({batman: ''}, "batman"))
+assert.ok('superman' in { superman: ''}) // it's not so clear that we are searching for an Object key
+assert.ok(Reflect.has({batman: ''}, "batman")) // now it is!
 //  --- has
 
 // --- ownKeys
@@ -81,16 +78,19 @@ const user = Symbol('user')
 const databaseUser = {
     id: 1,
     [Symbol.for('password')]: 123,
-    [user]: 'erickwendel'
+    [user]: 'leandropassos'
 }
 
-// Com os metodos de object, temos que fazer 2 requisicoes
+// Through Object's methods we must do 3 requests
 const objectKeys = [
     ...Object.getOwnPropertyNames(databaseUser),
-    ...Object.getOwnPropertySymbols(databaseUser),
-]
-assert.deepStrictEqual(objectKeys, ['id', Symbol.for('password'), user])
-
-// com reflection, só um método
-assert.deepStrictEqual(Reflect.ownKeys(databaseUser), ['id', Symbol.for('password'), user])
+    // we need this next line, otherwise it will not return the Symbol (which is part of the properties)
+    ...Object.getOwnPropertySymbols(databaseUser), 
+];
+// console.log('databaseUser', databaseUser);
+// console.log('objectKeys', objectKeys);
+assert.deepStrictEqual(objectKeys, ['id', Symbol.for('password'), user]);
+// using Reflection, only one method
+// console.log('Reflect.ownKeys', Reflect.ownKeys(databaseUser));
+assert.deepStrictEqual(Reflect.ownKeys(databaseUser), ['id', Symbol.for('password'), user]);
 // --- ownKeys
