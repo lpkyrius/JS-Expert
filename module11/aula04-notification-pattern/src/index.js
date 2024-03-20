71138761314ebf4dbbcb5c8be12368e5ca0a4e06
lpkyrius <lpkyrius@hotmail.com>
2024-03-20 19:10:23 +0000

    module11 aula04 notification types

---
+++ module11/aula04-notification-pattern/src/index.js
@@ -0,0 +1,36 @@
+import { createServer } from 'http'
+import { statusCodes } from '../src/util/httpStatusCodes.js'
+import HeroEntity from './util/heroEntity.js'
+
+async function handler(request, response) {
+    for await (const data of request) {
+        try {
+            const parsedData = JSON.parse(data)
+            // to simulate another error such as DB error
+            if (Reflect.has(parsedData, "connectionError")) {
+                // generic error for any other unexpected scenario 
+                throw new Error("error connecting to database!")
+            }
+            const hero = new HeroEntity(parsedData)
+            if (!hero.isValid()) {
+                response.writeHead(statusCodes.BAD_REQUEST)
+                response.end(hero.notification.join('\n')) // error.message because we've populated the msg with a custom message
+                continue; // to continue inside the 'For' above
+            }
+
+            // Save into database
+            response.writeHead(statusCodes.OK)
+            response.end()
+
+        } catch (error) {
+            response.writeHead(statusCodes.INTERNAL_SERVER_ERROR)
+            response.end()
+        }
+    }
+}
+
+createServer(handler).listen(3000, () => console.log('running at 3000'))
+
+/*
+    curl -i localhost:3000 -X POST --data '{"name": "Avenger", "age": 80}'
+*/
