e10cf6bf5fe361dad605b834a8e01ec085d73a9a
lpkyrius <lpkyrius@hotmail.com>
2024-04-01 17:57:44 +0100

    Module13 Child Process

---
+++ module13/01-child-process/01-spawn-process/server.js
@@ -0,0 +1,18 @@
+import { hasSubscribers } from 'diagnostics_channel'
+import { createServer } from 'http'
+import { randomUUID } from 'crypto'
+import { pipeline } from 'stream/promises'
+import { createWriteStream } from 'fs'
+
+async function handler(request, response) {
+    const fileName = `file-${randomUUID()}.csv`
+    await pipeline(
+        request,
+        createWriteStream(fileName)
+    )
+
+    response.end('uploaded with success')
+}
+
+createServer(handler)
+.listen(3000, () => console.log('running at 3000'))
