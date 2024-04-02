48188da9ccea5b86fb15ba07726d5580b093dd1d
lpkyrius <lpkyrius@hotmail.com>
2024-04-02 16:15:52 +0100

    Module 13 - 02 process report

--- module13/01-child-process/02-process-report/src/backgroundTask.js
+++ module13/01-child-process/02-process-report/src/backgroundTask.js
@@ -1,3 +1,47 @@
-process.on('message', (msg) => console.log('msg from child', msg.Name))
+import { createReadStream } from 'fs'
+import { pipeline } from 'stream/promises'
+import { Writable, Transform } from 'stream'
+import csvtojson from 'csvtojson'
+import { setTimeout } from 'timers/promises'
 
-// console.log(`I'm ready!!! Child Process: ${process.pid}`)
+const database = process.argv[2]
+
+async function onMessage(msg) {
+    const firstTimeRan = []
+
+    await pipeline(
+        createReadStream(database),
+        csvtojson(),
+        Transform({
+            transform(chunk, enc, cb) {
+                const data = JSON.parse(chunk)
+                // if not duplicated, exit
+                if (data.Name !== msg.Name) return cb()
+
+                if(firstTimeRan.includes(msg.Name)) {
+                    return cb(null, msg.Name)
+                }
+                
+                firstTimeRan.push(msg.Name)
+                cb()
+            }
+        }),
+        Writable({
+            write(chunk, enc, cb) {
+                // if empty chunk, return 
+                if(!chunk) return cb()
+
+                process.send(chunk.toString())
+            }
+        })
+    )
+}
+
+// process.on('message', (msg) => console.log('msg from child', msg.Name))
+process.on('message', onMessage)
+
+// console.log(`I'm ready!!! Child Process: ${process.pid}`, database)
+
+// to say the sub process can be killed for inactivity after 5 secs
+await setTimeout(10000)
+process.channel.unref()
--- module13/01-child-process/02-process-report/src/index.js
+++ module13/01-child-process/02-process-report/src/index.js
@@ -4,14 +4,32 @@ import { pipeline } from 'stream/promises'
 import { Writable } from 'stream'
 import csvtojson from 'csvtojson'
 
-const database = './data/All_Pokemon2.csv'
-const PROCESS_COUNT = 10;
+// const database = './data/All_Pokemon2.csv'
+const database = './data/All_Pokemon.csv'
+const PROCESS_COUNT = 30;
+const replications = []
 
 const backgroundTaskFile = './src/backgroundTask.js'
 const processes = new Map()
 // first we instance every process, then we sent it forward
 for(let index=0; index<PROCESS_COUNT; index++) {
     const child = fork(backgroundTaskFile, [database])
+    child.on('exit', () => {
+        console.log(`process ${child.pid} exited`)
+        processes.delete(child.pid)
+    })
+    child.on('error', error => {
+        console.log(`process ${child.pid} has an error`, error)
+        processes.exit(1)
+    })
+
+    child.on('message', msg => {
+        // work around for multiprocessing
+        if(replications.includes(msg)) return ;
+
+        console.log(`${msg} is replicated`)
+        replications.push(msg)
+    })
     processes.set(child.pid, child)
 }
 
